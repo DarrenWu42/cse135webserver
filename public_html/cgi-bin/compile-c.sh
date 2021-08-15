0047e22963cfd08068b7fdf0876b635e50f25992
fborealis <wudarren0908@gmail.com>
2021-08-15 12:38:04 -0700

    Fixed filenames and added php to cgi bin

--- public_html/cgi-bin/compile-c.sh
+++ public_html/cgi-bin/compile-c.sh
@@ -2,8 +2,8 @@ gcc ${1}c-destroy-session.c -o ${1}c-destroy-session.cgi
 gcc ${1}c-env.c -o ${1}c-env.cgi
 gcc ${1}c-general-request-echo.c -o ${1}c-general-request-echo.cgi
 gcc ${1}c-get-echo.c -o ${1}c-get-echo.cgi
-gcc ${1}c-hello-html-world.c -o ${1}c-hello-html-world.cgi
-gcc ${1}c-hello-json-world.c -o ${1}c-hello-json-world.cgi
+gcc ${1}c-hello-html.c -o ${1}c-hello-html.cgi
+gcc ${1}c-hello-json.c -o ${1}c-hello-json.cgi
 gcc ${1}c-post-echo.c -o ${1}c-post-echo.cgi
 gcc ${1}c-sessions-1.c -o ${1}c-sessions-1.cgi
 gcc ${1}c-sessions-2.c -o ${1}c-sessions-2.cgi
---
+++ public_html/cgi-bin/php-hello-html.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>Hello, PHP!</title>
+    </head>
+    <body>
+        <?php 
+            echo "<h1 style='align:center;'>Hello, PHP!</h1>";
+        ?>
+    </body>
+</html>
