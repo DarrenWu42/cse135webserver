83b25092ffabdebbc611e678b1557f95a2bc2a33
fborealis <wudarren0908@gmail.com>
2021-08-15 23:47:22 -0700

    py sess 1

--- public_html/cgi-bin/python/python-sessions-1.py
+++ public_html/cgi-bin/python/python-sessions-1.py
@@ -13,7 +13,7 @@ if(username == ""): # if username from form is empty
     cookie_string = os.environ.get('HTTP_COOKIE') # load cookie string from environ
     cookie.load(cookie_string)                    # turn cookie string to dict
     username = cookie['username'].value           # get username value from cookie string
-    username = "None" if username == "" else username # if username is still empty, set it to none
+    # username = "None" if username == "" else username # if username is still empty, set it to none
 else: # else if username from form is not empty
     cookie['username'] = username # set cookie to username
     print(cookie)                 # print header to set cookie
