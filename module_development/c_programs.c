8e8fa9cbadf01dc0cad22d877a91be57ff5d2d2f
Darren Wu <wudarren0908@gmail.com>
2021-08-23 13:33:10 -0700

    apache mod: fixed failure page

--- module_development/c_programs.c
+++ module_development/c_programs.c
@@ -86,7 +86,8 @@ static int page_caller(request_rec *r){
             <body><h1 align=center>Apache Module Error!</h1>\
             <hr/>\n");
 
-        ap_rprintf(r, strcat(filename,"<br/>\n"));
+        ap_rprintf(r, filename);
+        ap_rprintf("<br/>\n");
         ap_rprintf(r, "</body></html>");
         return OK;
     }
