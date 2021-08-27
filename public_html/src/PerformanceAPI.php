e40f7bc9c10755f84194b2f1bd80ec0d513c23c8
Darren Wu <wudarren0908@gmail.com>
2021-08-27 10:44:42 -0700

    testing attempt 2

--- public_html/src/PerformanceAPI.php
+++ public_html/src/PerformanceAPI.php
@@ -202,7 +202,7 @@ class PerformanceAPI {
             'decoded_body_size' => ((int) $input['decodedBodySize']) ?? null
         ];
 
-        fwrite(STDOUT, var_dump($input_array));
+        fwrite(STDOUT, var_export($input_array, true));
         return $input_array;
     }
 
