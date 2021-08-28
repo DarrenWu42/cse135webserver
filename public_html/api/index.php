804dded517d181161eedc9b070e9d188d7f839bb
Darren Wu <wudarren0908@gmail.com>
2021-08-27 21:50:51 -0700

    added pdo error enable

--- public_html/api/index.php
+++ public_html/api/index.php
@@ -5,6 +5,8 @@ use Src\StaticAPI;
 use Src\PerformanceAPI;
 use Src\ActivityAPI;
 
+$dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
