60572a9de643b2a767bb3464af3b92f1097d5f56
Darren Wu <wudarren0908@gmail.com>
2021-09-02 18:11:42 -0700

    include patch with allow methods

--- REST_API/api/index.php
+++ REST_API/api/index.php
@@ -10,7 +10,7 @@ $dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
+header("Access-Control-Allow-Methods: OPTIONS,GET,PATCH,POST,PUT,DELETE");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
