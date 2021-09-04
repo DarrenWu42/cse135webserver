1250d2bdd255f04ce6d1fb20462e70c2d09a03b2
Darren Wu <wudarren0908@gmail.com>
2021-09-03 23:40:18 -0700

    asdf

--- reporting_html/dashboard.js
+++ reporting_html/dashboard.js
@@ -51,43 +51,12 @@ var baseConfigPie = {
     title: {
         text: '',
         align: 'center',
-        offsetX: 10,
         fontSize: 25
     },
     legend: {
         backgroundColor: '#eee',
         fontSize: '1.5em',
         width: '150em'
-    },
-    plot:{
-        detach: false
-    },
-    series:null
-};
-
-var baseConfigRing = {
-    type: 'ring',
-    title: {
-        text: '',
-        align: 'center',
-        offsetX: 10,
-        fontSize: 25
-    },
-    valueBox: {
-        placement: 'out',
-        text: '%t\n%npv%',
-        fontFamily: 'Open Sans'
-    },
-    legend: {
-        backgroundColor: '#eee',
-        fontSize: '1.5em',
-        width: '150em'
-    },
-    tooltip: {
-        fontSize: '18',
-        fontFamily: 'Open Sans',
-        padding: '5 10',
-        text: '%npv%'
     },
     plot:{
         detach: false
@@ -112,8 +81,6 @@ function loadTimesChart(){
             loadTimesValues[3]++;
     }
 
-    loadTimesValues = loadTimesValues.map(x => (x/durations.length)*100);
-
     for(var i = 0; i < loadTimesValues.length; i++){
         loadTimesData[i].values=[loadTimesValues[i]];
         if(loadTimesValues[i] == 0){
@@ -128,30 +95,44 @@ function loadTimesChart(){
     zingchart.render({
         id: 'loadTimesChart',
         data: baseConfigPie,
+        height: '400px',
+        width: '400px'
     });
 }
 
 function connectionsChart(){
-    let connectionsData = [{values:[0],text:"Slow 2g",backgroundColor:"#ff6969"},
-                         {values:[0],text:"2g",backgroundColor:"#d17719"},
-                         {values:[0],text:"3g",backgroundColor:"#888500"},
-                         {values:[0],text:"4g",backgroundColor:"#00880B"}];
+    let connectionsData = [{text:"Slow 2g",backgroundColor:"#ff6969"},
+                         {text:"2g",backgroundColor:"#d17719"},
+                         {text:"3g",backgroundColor:"#888500"},
+                         {text:"4g",backgroundColor:"#00880B"}];
+    let connectionsValues = [0,0,0,0];
     for(const connection_type of connection_types){
         if(connection_type == "slow-2g")
-            connectionsData[0].values[0]++;
+            connectionsValues[0]++;
         else if(connection_type == "2g")
-            connectionsData[1].values[0]++;
+            connectionsValues[1]++;
         else if(connection_type == "3g")
-            connectionsData[2].values[0]++;
+            connectionsValues[2]++;
         else
-            connectionsData[3].values[0]++;
+            connectionsValues[3]++;
+    }
+
+    for(var i = 0; i < connectionsValues.length; i++){
+        connectionsData[i].values=[connectionsValues[i]];
+        if(connectionsValues[i] == 0){
+            connectionsData.splice(i, 1);
+            connectionsValues.splice(i, 1);
+            i--;
+        }
     }
     
     baseConfigPie.series = connectionsData;
     
     zingchart.render({
         id: 'connectionsChart',
-        data: baseConfigPie
+        data: baseConfigPie,
+        height: '400px',
+        width: '400px'
     });
 }
 
@@ -168,22 +149,20 @@ function languagesChart(){
             languagesCounts.push(1);
         }
     }
-
-    languagesCounts = languagesCounts.map(x => (x/languages.length)*100);
-
-    let lowerValue = languagesCounts.length < 7 ? languagesCounts.length : 7;
     
     for(var i = 0; i < lowerValue; i++){
         languagesData[i]={value:[languagesCounts[i]],
                           text:languagesValues[i],
-                          backgroundColor:colors[i]};
+                          backgroundColor:colors[i%7]};
     }
     
     baseConfigPie.series = languagesData;
     
     zingchart.render({
         id: 'languagesChart',
-        data: baseConfigPie
+        data: baseConfigPie,
+        height: '400px',
+        width: '400px'
     });
 }
 
